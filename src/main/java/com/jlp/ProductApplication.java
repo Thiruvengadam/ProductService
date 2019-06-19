6874cced0d94c9c13e6a517cb06b0d4340915c32
Amirtha <amirthavalli.ramanujam@rntbci.com>
2019-06-19 21:36:14 +0530

    Test commit

--- src/main/java/com/jlp/ProductApplication.java
+++ src/main/java/com/jlp/ProductApplication.java
@@ -15,6 +15,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class ProductApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProductApplication.class, args);
+        SpringApplication.run(ProductApplication.class, args); 
     }
 }
